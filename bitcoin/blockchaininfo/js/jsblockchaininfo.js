00eeecbe95e2b99d0cbc637a36f0ebbec71535b2
antoninbrunel <antonin.brunel@gmail.com>
2017-03-27 14:07:55 +0200

    Add files via upload

---
+++ bitcoin/blockchaininfo/js/jsblockchaininfo.js
@@ -0,0 +1,23 @@
+function homePageLoading()
+{
+	url='https://bitcoin.mubiz.com/blockchaininfo'; 
+	elementID='demo';
+	fonctionRequeteApi(url,elementID);
+	
+}
+function fonctionRequeteApi (url,elementID)
+{
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() 
+{
+    if (this.readyState == 4 && this.status == 200) 
+	{
+        var myObj = this.responseText;
+        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+        document.getElementById(elementID).innerHTML = jsonPretty;
+    }
+
+};
+xmlhttp.open("GET",url, true);
+xmlhttp.send();	
+}
---
+++ js/custom.js
@@ -0,0 +1,18 @@
+
+function fonctionRequeteApi (url,elementID)
+{
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() 
+{
+    if (this.readyState == 4 && this.status == 200) 
+	{
+        var myObj = this.responseText;
+        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+        document.getElementById(elementID).innerHTML = jsonPretty;
+    }
+
+};
+xmlhttp.open("GET",url, true);
+xmlhttp.send();	
+}
+
